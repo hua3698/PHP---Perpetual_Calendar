a5b4ea68600e1989fec4d62ac2f6061eade81a78
hua3698 <a29424315@gmail.com>
2020-11-10 16:36:28 +0800

    hover

--- calendar.php
+++ calendar.php
@@ -1,3 +1,9 @@
+ <!--
+ * 目標功能 
+1. 連結動畫效果
+2. today區隔
+3. 邊緣修圓角 --> 
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,10 +14,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <style>
-        .container {
+        .box {
             width: 500px;
             margin: auto;
         }
@@ -24,15 +29,9 @@
         table td {
             width: 80px;
             text-align: center;
-            padding: 5px;
+            padding: 3px;
             border-radius: 50%;
         }
-
-        /* table td:hover {
-            background: #ccc;
-            color: #fff;
-        } */
-
         .pic {
             width: 500px;
             height: 250px;
@@ -45,31 +44,65 @@
         }
 
         .weekend {
-            color: #ffa5a5;
+            color: #f94949;
         }
 
         .today {
             background: #ffa5a5;
         }
-
+        .th{
+            width: 45px;
+            height: 45px;
+            color:#6ba6e6; 
+            font-weight: bold; 
+            margin:15px;
+        }
         .cr {
-            width: 50px;
-            height: 50px;
+            width: 45px;
+            height: 45px;
             border-radius: 50%;
             display: flex;
             justify-content: center;
-            align-items: center;
+            align-items: start;
+            transform: translateX(20%);
+            user-select: none;
         }
         .cr:hover {
             /* border: 1px solid #ccc; */
             background: #ffe79be3;
+            color: #f9f9f9 !important;
+            
+        }
+         .animate{
+            animation: arrowA 2s infinite; 
+
+            /* animation-fill-mode: forwards; */
+         }
+         .animateB{
+            animation: arrowB 2s infinite;
+            /* animation-fill-mode: forwards; */
+         }
+        @keyframes arrowA {
+            50%{
+                transform: translateX(-10%);
+            }
+            100%{
+                transform: translateX(0%);
+            }
+        }
+        @keyframes arrowB {
+            50%{
+                transform: translateX(10%);
+            }
+            100%{
+                transform: translateX(0%);
+            }
         }
     </style>
 </head>
 
 <body class="alert-warning p-5">
     <?php
-    date_default_timezone_set("Asia/Taipei");
 
     if (isset($_GET['y']) && isset($_GET['m'])) {
         $year = $_GET['y'];
@@ -115,78 +148,78 @@
     // echo "當月第一天是星期幾：" . $startDay . "<br>";
     ?>
 
-    <div class="box">
-        <a href="calendar.php?y=<?= $lastYear ?>&m=<?= $lastMonth ?>">
-            < Last Month</a> <a href="calendar.php?y=<?= $nextYear ?>&m=<?= $nextMonth ?>">Next Month >
-        </a>
-        <div class="container">
+    <div class="container d-flex align-items-center justify-content-around">
+        <a href="calendar.php?y=<?= $lastYear ?>&m=<?= $lastMonth ?>" class="animate">< Last Month</a> 
+        <div class="box">
             <div class="pic pl-3 shadow rounded-top">
                 <div style="font-size: 6rem;">
-                    <?php
-                    echo date("d");
-                    // if(empty($_GET['d'])){
-                    //     echo date("d");
-                    // }
-                    ?>
+                        <?php
+                        echo date("d");
+                        // if(empty($_GET['d'])){
+                        //     echo date("d");
+                        // }
+                        ?>
                 </div>
                 <div style="font-size: 2.5rem;">
-                    <?php echo date("F , Y", strtotime($firstDay)); ?>
+                        <?php echo date("F , Y", strtotime($firstDay)); ?>
                 </div>
             </div>
             <div class="">
                 <table class="rounded-bottom shadow">
                     <tr>
-                        <td style="color:#6ba6e6; font-weight: bold;">Sun</td>
-                        <td style="color:#6ba6e6; font-weight: bold;">Mon</td>
-                        <td style="color:#6ba6e6; font-weight: bold;">Tue</td>
-                        <td style="color:#6ba6e6; font-weight: bold;">Wed</td>
-                        <td style="color:#6ba6e6; font-weight: bold;">Thu</td>
-                        <td style="color:#6ba6e6; font-weight: bold;">Fri</td>
-                        <td style="color:#6ba6e6; font-weight: bold;">Sat</td>
+                        <td class="th" >Sun</td>
+                        <td class="th">Mon</td>
+                        <td class="th">Tue</td>
+                        <td class="th">Wed</td>
+                        <td class="th">Thu</td>
+                        <td class="th">Fri</td>
+                        <td class="th">Sat</td>
                     </tr>
                     <?php
-                    $holiday = [
-                        '2-28'=> '和平紀念日',
-                        '10-10' => '雙十節',
-                        '10-30' => '萬聖節',
-                        '12-25' => '聖誕節'
-                    ];
+                        $holiday = [
+                            '2-28' => '和平紀念日',
+                            '10-10' => '雙十節',
+                            '10-30' => '萬聖節',
+                            '12-25' => '聖誕節'
+                        ];
 
-                    for ($i = 0; $i < 6; $i++) {
-                        echo "<tr>";
-                        for ($j = 0; $j < 7; $j++) {
-                            echo "<td>";
-                            echo "<div class='cr'>";
-                            if ($j == 0 || $j == 6) {
-                                echo "<div class='weekend'>";
+                        for ($i = 0; $i < 6; $i++) {
+                            echo "<tr>";
+                            for ($j = 0; $j < 7; $j++) {
+                                echo "<td>";
+                                echo "<div class='cr'>";
+                                if ($j == 0 || $j == 6) {
+                                    echo "<div class='weekend'>";
+                                }
+                                $date = "";
+
+                                if ($j < $startDay && $i == 0) {
+                                    echo  "&nbsp";
+                                } else if ((($i * 7) + ($j + 1) - $startDay) > $monthDay) {
+                                    // echo  "&nbsp";
+                                    break;
+                                } else {
+                                    $date = (($i * 7) + ($j + 1) - $startDay);
+                                }
+                                echo  $date;
+
+                                if (!empty($holiday[$month . '-' . $date])) {
+                                    echo "<br>" . $holiday[$month . '-' . $date];
+                                };
+                                // if ($date == date("d")) {
+                                //     echo "<div class='today'>" . date('d') . "</div>";  #adbdf9
+                                // }
+                                echo "</div>";
+                                echo "</div>";
+                                echo "</td>";
                             }
-                            $date = "";
-
-                            if ($j < $startDay && $i == 0) {
-                                echo  "&nbsp";
-                            } else if ((($i * 7) + ($j + 1) - $startDay) > $monthDay) {
-                                // echo  "&nbsp";
-                                break;
-                            } else {
-                                $date = (($i * 7) + ($j + 1) - $startDay);
-                            }
-                            echo  $date;
-
-                            if (!empty($holiday[$month . '-' . $date])) {
-                                echo "<br>" . $holiday[$month . '-' . $date];
-                            };
-                            // if ($date == date("d")) {
-                            //     echo "<div class='today'>" . date('d') . "</div>";  #adbdf9
-                            // }
-                            echo "</div>";
-                            echo "</td>";
+                            echo "</tr>";
                         }
-                        echo "</tr>";
-                    }
                     ?>
                 </table>
             </div>
         </div>
+        <a href="calendar.php?y=<?= $nextYear ?>&m=<?= $nextMonth ?>" class="animateB">Next Month ></a>
     </div>
 </body>
 
