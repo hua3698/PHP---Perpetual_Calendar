193fa17a6fd5312dd1edd45f815eca721467fdab
hua3698 <a29424315@gmail.com>
2020-11-11 12:49:38 +0800

    123

--- calendar.php
+++ calendar.php
@@ -1,8 +1,10 @@
- <!--
+<!--
  * 目標功能 
-1. 連結動畫效果
-2. today區隔
-3. 邊緣修圓角 --> 
+1. 連結動畫效果 ok
+2. 改變today底色
+3. 邊緣修圓角 ok 
+4. 節日位置調整
+5. 練習RWD效果     -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,26 +14,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <style>
         .box {
             width: 500px;
             margin: auto;
+            border-radius: 20px;
         }
 
         table {
             width: 500px;
             background: #eee;
+            border-bottom-left-radius: 20px;
+            border-bottom-right-radius: 20px;
         }
 
         table td {
-            width: 80px;
             text-align: center;
-            padding: 3px;
+            /* padding: 3px; */
             border-radius: 50%;
         }
+
         .pic {
             width: 500px;
             height: 250px;
@@ -41,6 +46,8 @@
             opacity: 0.8;
             color: #3e62e6;
             text-shadow: 3px 3px 0px #fff;
+            border-top-left-radius: 20px;
+            border-top-right-radius: 20px;
         }
 
         .weekend {
@@ -50,53 +57,66 @@
         .today {
             background: #ffa5a5;
         }
-        .th{
+
+        .th {
             width: 45px;
             height: 45px;
-            color:#6ba6e6; 
-            font-weight: bold; 
-            margin:15px;
+            color: #6ba6e6;
+            font-weight: bold;
         }
+
         .cr {
             width: 45px;
             height: 45px;
             border-radius: 50%;
             display: flex;
             justify-content: center;
-            align-items: start;
-            transform: translateX(20%);
+            align-items: center;
+            transform: translateX(25%);
             user-select: none;
+            position: relative;
         }
+
         .cr:hover {
-            /* border: 1px solid #ccc; */
             background: #ffe79be3;
-            color: #f9f9f9 !important;
+        }
+
+        .animate {
+            animation: arrowA 2s infinite;
             
         }
-         .animate{
-            animation: arrowA 2s infinite; 
 
-            /* animation-fill-mode: forwards; */
-         }
-         .animateB{
+        .animateB {
             animation: arrowB 2s infinite;
             /* animation-fill-mode: forwards; */
-         }
+        }
+
         @keyframes arrowA {
-            50%{
-                transform: translateX(-10%);
+            50% {
+                transform: translateX(-5%);
             }
-            100%{
+
+            100% {
                 transform: translateX(0%);
             }
         }
+
         @keyframes arrowB {
-            50%{
-                transform: translateX(10%);
+            50% {
+                transform: translateX(5%);
             }
-            100%{
+
+            100% {
                 transform: translateX(0%);
             }
+        }
+
+        .hol {
+            font-size: 5px;
+            position: absolute;
+            top: 70%;
+            left: 10%;
+            color: #9e9e9e;
         }
     </style>
 </head>
@@ -149,37 +169,41 @@
     ?>
 
     <div class="container d-flex align-items-center justify-content-around">
-        <a href="calendar.php?y=<?= $lastYear ?>&m=<?= $lastMonth ?>" class="animate">< Last Month</a> 
-        <div class="box">
-            <div class="pic pl-3 shadow rounded-top">
-                <div style="font-size: 6rem;">
+        <a href="calendar.php?y=<?= $lastYear ?>&m=<?= $lastMonth ?>" class="animate" style="text-decoration: none;"><< Last Month</a> 
+        <div class="box shadow-lg my-5">
+                <div class="pic pl-4">
+                    <div style="font-size: 6rem;">
                         <?php
                         echo date("d");
-                        // if(empty($_GET['d'])){
-                        //     echo date("d");
-                        // }
                         ?>
-                </div>
-                <div style="font-size: 2.5rem;">
+                    </div>
+                    <div style="font-size: 2.5rem;">
                         <?php echo date("F , Y", strtotime($firstDay)); ?>
+                    </div>
                 </div>
-            </div>
-            <div class="">
-                <table class="rounded-bottom shadow">
-                    <tr>
-                        <td class="th" >Sun</td>
-                        <td class="th">Mon</td>
-                        <td class="th">Tue</td>
-                        <td class="th">Wed</td>
-                        <td class="th">Thu</td>
-                        <td class="th">Fri</td>
-                        <td class="th">Sat</td>
-                    </tr>
-                    <?php
+                <div class="">
+                    <table class="">
+                        <tr>
+                            <td class="th">Sun</td>
+                            <td class="th">Mon</td>
+                            <td class="th">Tue</td>
+                            <td class="th">Wed</td>
+                            <td class="th">Thu</td>
+                            <td class="th">Fri</td>
+                            <td class="th">Sat</td>
+                        </tr>
+                        <?php
                         $holiday = [
+                            '1-1'=>'元旦',
                             '2-28' => '和平紀念日',
+                            '3-8'=>'婦女節',
+                            '4-1' => '愚人節',
+                            '4-4'=>'兒童節',
+                            '5-15' => '破蛋日',
+                            '9-28' => '教師節',
                             '10-10' => '雙十節',
                             '10-30' => '萬聖節',
+                            '11-11' => '光棍節',
                             '12-25' => '聖誕節'
                         ];
 
@@ -191,8 +215,8 @@
                                 if ($j == 0 || $j == 6) {
                                     echo "<div class='weekend'>";
                                 }
-                                $date = "";
 
+                                $date = "";
                                 if ($j < $startDay && $i == 0) {
                                     echo  "&nbsp";
                                 } else if ((($i * 7) + ($j + 1) - $startDay) > $monthDay) {
@@ -204,22 +228,21 @@
                                 echo  $date;
 
                                 if (!empty($holiday[$month . '-' . $date])) {
-                                    echo "<br>" . $holiday[$month . '-' . $date];
-                                };
-                                // if ($date == date("d")) {
-                                //     echo "<div class='today'>" . date('d') . "</div>";  #adbdf9
-                                // }
-                                echo "</div>";
+                                    echo "<div class='hol'>" . $holiday[$month . '-' . $date] . "</div>";
+                                }
+                                if ($date == date("d")) {
+                                echo "<div class='today bg-dark'></div>";  #adbdf9
+                                }
                                 echo "</div>";
                                 echo "</td>";
                             }
                             echo "</tr>";
                         }
-                    ?>
-                </table>
-            </div>
-        </div>
-        <a href="calendar.php?y=<?= $nextYear ?>&m=<?= $nextMonth ?>" class="animateB">Next Month ></a>
+                        ?>
+                    </table>
+                </div>
+    </div>
+    <a href="calendar.php?y=<?= $nextYear ?>&m=<?= $nextMonth ?>" class="animateB" style="text-decoration: none;">Next Month >></a>
     </div>
 </body>
 
